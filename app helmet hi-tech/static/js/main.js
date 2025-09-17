b7404c357f2aef40a136605e63cc0384ffd36a77
catatall888 <catatall677@gmail.com>
2025-09-16 21:51:10 -0300

    Delete app helmet hi-tech directory

--- app helmet hi-tech/static/js/main.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("menu-toggle");
-const sidebar = document.getElementById("sidebar");
-
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
